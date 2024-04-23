3f5fa3a2a59903567be531db8e4260b69917ec76
Perttu Nurmi <perttu.nurmi+github@gmail.com>
2024-04-23 09:00:44 -0400

    sway

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -3,9 +3,9 @@ do
     date=$(date +'%A, %b %d')
     time=$(date +'%I:%M %p')
     weather=$(cat '/tmp/weather') 
-    cputemp=$(cat /sys/devices/pci0000\:00/0000\:00\:18.3/hwmon/hwmon4/temp1_input | cut -c 1-2)
+    battery=$(cat "/sys/class/power_supply/BAT0/capacity")
 
-    echo " | $cputemp°C | $weather | $date | $time | "
+    echo " | BAT:$battery% | $weather | $date | $time | "
     sleep 6
 
 done 
