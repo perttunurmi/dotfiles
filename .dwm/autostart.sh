fe91dd17bb2479e46b4bd7f8a7c9396b27d4ce9e
perttunurmi <perttu.nurmi@gmail.com>
2024-11-02 15:47:20 +0200

    autostart

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,7 +4,8 @@ keepassxc &
 volumeicon &
 flameshot &
 echo 'auto' > '/sys/bus/usb/devices/1-10/power/control'
+xmodmap -e "clear Lock" -e "keycode 0x42 = Escape"
 ~/.fehbg &
 # ~/workspaces/dotfiles/scripts/performance.sh
 
-while true; do xsetroot -name "[$(powerprofilesctl get)] [$(cat /sys/class/power_supply/BAT0/capacity)%] [$(date +"%F")] [$(date +"%H:%M")] "; sleep 1s; done &
+while true; do xsetroot -name "[$(powerprofilesctl get)][$(cat /sys/class/power_supply/BAT0/capacity)%][$(date +"%F")][$(date +"%H:%M")]"; sleep 1s; done &
