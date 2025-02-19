7c29af4193e64914ca8bb24a0cb195bac4a9b738
perttunurmi <perttu.nurmi@gmail.com>
2025-02-19 09:09:16 +0200

    wakatime

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,6 +1,7 @@
 return {
     { 'HiPhish/rainbow-delimiters.nvim' },
     { 'nvim-treesitter/nvim-treesitter-context' },
+    { 'wakatime/vim-wakatime', lazy = false },
     {
         'tpope/vim-fugitive',
         vim.keymap.set('n', '<leader>g', vim.cmd.Git),
