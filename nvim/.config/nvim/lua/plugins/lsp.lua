f7560c6f02cd5aec24eac1b3cc9e1a9be6d09e63
perttunurmi <perttu.nurmi@gmail.com>
2025-03-24 07:33:34 +0200

    update

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -144,6 +144,7 @@ return {
                 css_variables = {},
                 cssmodules_ls = {},
                 marksman = {},
+                eslint = {},
 
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 -- Some languages (like typescript) have entire language plugins that can be useful:
