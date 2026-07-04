8c9cd23af6a00be47e334ee884b79cab1017d5e3
Perttu Nurmi <perttu.nurmi@gmail.com>
2026-07-04 15:35:11 +0300

    Add bash folder
    Signed-off-by: Perttu Nurmi <perttu.nurmi@gmail.com>

---
+++ bash/scripts/tmux.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [[ ! -x $(which fzf) ]]; then
+    printf "fzf not installed\n"
+    exit 1
+fi
+
+if [[ ! -x $(which tmux) ]]; then
+    printf "tmux not installed\n"
+    exit 1
+fi
+
+tmux_fzf_select_session() {
+    ghost="Select a session to attach to or cancel by pressing ctrl-c"
+    selected_session=$(tmux list-sessions | fzf --ghost="$ghost" | sed -E 's/: .*//')
+
+    if [[ -n $selected_session ]]; then
+        tmux attach-session -t "$selected_session"
+    fi
+}
+
+tmux_fzf_select_session
